1a304b2b258da901fbe1ad0f2b212d54e55c2d52
codexmish <majharulislam.shanto.70@gmail.com>
2025-09-17 18:53:33 +0600

    layOut done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import LayoutOne from './Layouts/LayoutOne'
+import Home from './pages/Home'
 
 const App = () => {
+
+  const myRoute = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<LayoutOne/>}>
+      <Route index element={<Home/>}/>
+
+
+    </Route>
+  ))
+
+
   return (
     <>
+    <RouterProvider router={myRoute}/>
     
     </>
   )
