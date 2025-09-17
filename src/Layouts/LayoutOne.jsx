99d295ad8d42f75054101aea24491267f20d5d14
codexmish <majharulislam.shanto.70@gmail.com>
2025-09-17 18:55:19 +0600

    some changes

--- src/Layouts/LayoutOne.jsx
+++ src/Layouts/LayoutOne.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
 
 const LayoutOne = () => {
   return (
     <>
+    <Navbar/>
+    <Outlet/>
     
     </>
   )
