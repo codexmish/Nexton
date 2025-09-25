3f36b8f2b8ae37267117e88d2787aaa19e975b5a
codexmish <majharulislam.shanto.70@gmail.com>
2025-09-25 16:30:00 +0600

    banner section looading

---
+++ src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CommonBanner from './Common/CommonBanner'
+
+const Banner = () => {
+  return (
+    <>
+    <section id='banner'>
+        <CommonBanner/>
+
+    </section>
+
+    
+    </>
+  )
+}
+
+export default Banner
---
+++ src/components/Common/CommonBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommonBanner = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default CommonBanner
