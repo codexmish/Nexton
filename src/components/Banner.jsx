a53f7bb35cd2e94c21437d41264a3125cf0cef17
codexmish <majharulislam.shanto.70@gmail.com>
2025-09-25 17:32:55 +0600

    common banner failed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CommonBanner from './Common/CommonBanner'
+import back from '../assets/images/Banner-bg.png'
 
 const Banner = () => {
   return (
     <>
     <section id='banner'>
-        <CommonBanner/>
+
+        <CommonBanner bg={back}/>
 
     </section>
 
--- src/components/Common/CommonBanner.jsx
+++ src/components/Common/CommonBanner.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const CommonBanner = () => {
+const CommonBanner = ({bg}) => {
   return (
     <>
+    <div className={`${bg}`}>
+        <div className="container">
+        <h3 className='text-[20px] text-secoundary font-medium font-popppind'>Starting from: $49.99</h3>
+
+        </div>
+
+    </div>
     
     </>
   )
