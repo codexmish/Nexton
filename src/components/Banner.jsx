57230df9affc8c34cdb58b8333099c6698c1b963
codexmish <majharulislam.shanto.70@gmail.com>
2025-09-27 19:31:31 +0600

    banner added with swipper slider

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,56 @@
 import React from "react";
 import CommonBanner from "./Common/CommonBanner";
 import back from "../assets/images/hero.png";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
 
 const Banner = () => {
   return (
     <>
       <section id="banner" className="bg-[#f3f3f3]">
-        <div className="container">
-          <CommonBanner
-            hero={back}
-            textOne={"Starting from: $49.99"}
-            textTwo={"Exclusive collection for everyone"}
-          />
+        <div className="slide">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            pagination={{
+              dynamicBullets: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <CommonBanner
+                hero={back}
+                textOne={"Starting from: $49.99"}
+                textTwo={"Exclusive collection for everyone"}
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <CommonBanner
+                hero={back}
+                textOne={"Starting from: $49.99"}
+                textTwo={"Exclusive collection for everyone"}
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <CommonBanner
+                hero={back}
+                textOne={"Starting from: $49.99"}
+                textTwo={"Exclusive collection for everyone"}
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </section>
     </>
