f852a6ad0932b84375894d171b5fddbc717fd1a4
codexmish <majharulislam.shanto.70@gmail.com>
2025-09-27 18:37:40 +0600

    common banner added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import CommonBanner from './Common/CommonBanner'
-import back from '../assets/images/Banner-bg.png'
+import React from "react";
+import CommonBanner from "./Common/CommonBanner";
+import back from "../assets/images/hero.png";
 
 const Banner = () => {
   return (
     <>
-    <section id='banner'>
-
-        <CommonBanner bg={back}/>
-
-    </section>
-
-    
+      <section id="banner" className="bg-[#f3f3f3]">
+        <div className="container">
+          <CommonBanner
+            hero={back}
+            textOne={"Starting from: $49.99"}
+            textTwo={"Exclusive collection for everyone"}
+          />
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/Common/CommonBanner.jsx
+++ src/components/Common/CommonBanner.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import { CiSearch } from 'react-icons/ci'
-import { Link } from 'react-router'
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { Link } from "react-router";
 
-const CommonBanner = () => {
+const CommonBanner = ({ hero, textOne, textTwo }) => {
   return (
     <>
-    <div className=''>
-        <div className="container">
-        <h3 className='text-[20px] text-secoundary font-medium font-popppind'>Starting from: $49.99</h3>
-        <h2 className='w-[632px] text-[64px] text-primary font-semibold font-popppind'>Exclusive collection for everyone</h2>
-        <Link to={'/'} className='flex justify-center items-center bg-primary text-white w-[198px] h-[64px] rounded-[9999px] gap-3'>Explore now <CiSearch className='text-6xl text-whitetext'/></Link>
+      <div className="bg-[#f3f3f3] ">
+        <div className="container mx-6 lg:mx-0">
+          <div className="main-row flex justify-between items-center h-[550px] lg:h-fit">
+            <div className="text">
+              <h3 className="text-sm lg:text-[20px] text-secoundary font-medium font-popppind">
+                {textOne}
+              </h3>
+              <h2 className="w-[312px] lg:w-[632px] text-3xl lg:text-[64px] text-primary font-semibold font-popppind mt-2 mb-10 lg:my-6">
+                {textTwo}
+              </h2>
+              <Link
+                to={"/"}
+                className="flex justify-center items-center bg-primary text-white w-[198px] h-[64px] rounded-[9999px] gap-3"
+              >
+                Explore now <CiSearch className="text-6xl text-whitetext" />
+              </Link>
+            </div>
 
+            <div className="img hidden lg:block">
+              <img src={hero} alt="imgg" />
+            </div>
+          </div>
         </div>
-
-    </div>
-    
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default CommonBanner
+export default CommonBanner;
