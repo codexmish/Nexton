d8766adc90378c3a8e20489cedcf0b9286b33722
codexmish <majharulislam.shanto.70@gmail.com>
2025-09-28 22:22:27 +0600

    banner swiched to slick slider

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,57 +1,45 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
 import CommonBanner from "./Common/CommonBanner";
 import back from "../assets/images/hero.png";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-
-import "swiper/css/pagination";
-
-import { Pagination } from "swiper/modules";
+import Slider from "react-slick";
 
 const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
   return (
     <>
-      <section id="banner" className="bg-[#f3f3f3]">
-        <div className="slide">
-          <Swiper
-            spaceBetween={50}
-            slidesPerView={1}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
-            pagination={{
-              dynamicBullets: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <CommonBanner
-                hero={back}
-                textOne={"Starting from: $49.99"}
-                textTwo={"Exclusive collection for everyone"}
-              />
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <CommonBanner
-                hero={back}
-                textOne={"Starting from: $49.99"}
-                textTwo={"Exclusive collection for everyone"}
-              />
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <CommonBanner
-                hero={back}
-                textOne={"Starting from: $49.99"}
-                textTwo={"Exclusive collection for everyone"}
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
+      <section className="banner bg-[#f3f3f3]">
+        <Slider {...settings}>
+          <div>
+            <CommonBanner
+              hero={back}
+              textOne={"Starting from: $49.99"}
+              textTwo={"Exclusive collection for everyone"}
+            />
+          </div>
+          <div>
+            <CommonBanner
+              hero={back}
+              textOne={"Starting from: $49.99"}
+              textTwo={"Exclusive collection for everyone"}
+            />
+          </div>
+          <div>
+            <CommonBanner
+              hero={back}
+              textOne={"Starting from: $49.99"}
+              textTwo={"Exclusive collection for everyone"}
+            />
+          </div>
+        </Slider>
       </section>
     </>
   );
