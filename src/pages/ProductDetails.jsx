1328192823616d30d9469d4788679e151de9120d
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-01 12:11:24 +0600

    product details page res done

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -66,35 +66,36 @@ const ProductDetails = () => {
     <>
       <section id="productdetails" className="pt-10 pb-13">
         <div className="container">
-          <div className="product-row flex justify-between">
+          <div className="product-row flex justify-between flex-wrap px-3 lg:px-0">
             {/* ------------Product image part----- */}
             <div className="product-images flex gap-6 pb-13 border-b border-b-border flex-wrap ">
-              <div className="flex flex-col gap-4">
-                <button className="w-[140px] h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
-                <button className="w-[140px] h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
-                <button className="w-[140px] h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
-                <button className="w-[140px] h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
+              <div className="flex lg:flex-col gap-2 lg:gap-4 order-2 lg:order-0">
+                <button className="w-[70px] lg:w-[140px] h-[78px] lg:h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
+                <button className="w-[70px] lg:w-[140px] h-[78px] lg:h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
+                <button className="w-[70px] lg:w-[140px] h-[78px] lg:h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
+                <button className="w-[70px] lg:w-[140px] h-[78px] lg:h-[157px] rounded-2xl overflow-hidden bg-gray-200"></button>
               </div>
-              <div className="image w-[640px] h-[678px] bg-gray-200 rounded-2xl ">
+              <div className="image w-[312px] lg:w-[640px] h-[330px] lg:h-[678px] bg-gray-200 rounded-2xl">
                 {/* <img src="" alt="" /> */}
               </div>
             </div>
 
             {/* ------------Product options part----- */}
-            <div className="product-opt w-[460px] p-8 border border-[#E5E7EB] rounded-2xl h-fit">
-              <div className="flex justify-between items-start">
-                <div className="flex items-center gap-1">
+            <div className="product-opt w-[312px] lg:w-[460px] p-2 lg:p-8 lg:border border-[#E5E7EB] rounded-2xl h-fit">
+              <h2 className="text-2xl text-primary font-semibold font-popppind lg:hidden">Black Automatic Watch</h2>
+              <div className="flex justify-between items-start flex-wrap mt-3">
+                <div className="flex items-center gap-1 order-2 lg:order-0 w-[300px]">
                   <FaStar className="text-[#FBBF24]" />
-                  <p className="text-base text-secoundary font-semibold font-popppind">
-                    4.9 . 142 reviews
+                  <p className="text-[12px] lg:text-base text-secoundary font-semibold font-popppind">
+                    4.9 <span className="hidden lg:inline-block">. 142 reviews </span> <span className="lg:hidden">(98)</span>
                   </p>
                 </div>
 
-                <div className="price text-end">
-                  <h2 className="text-2xl text-primary font-semibold font-popppind">
+                <div className="price lg:text-end order-1 lg:order-0 mb-6 lg:mb-0">
+                  <h2 className="text-[20px] lg:text-2xl text-primary font-semibold font-popppind">
                     $169.99
                   </h2>
-                  <h3 className="text-sm text-secoundary font-medium font-popppind line-through">
+                  <h3 className="text-[12px] lg:text-sm text-secoundary font-medium font-popppind line-through">
                     $199.99
                   </h3>
                 </div>
@@ -106,20 +107,20 @@ const ProductDetails = () => {
               <h2 className="text-base text-primary font-semibold font-popppind">
                 Size: S
               </h2>
-              <div className="flex gap-2 mt-3 mb-8">
-                <button className="w-[72px] h-11 border border-[#E5E7EB] rounded-xl">
+              <div className="flex flex-wrap gap-2 mt-3 mb-8">
+                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-[#E5E7EB] rounded-xl">
                   S
                 </button>
-                <button className="w-[72px] h-11 border border-[#E5E7EB] rounded-xl">
+                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-[#E5E7EB] rounded-xl">
                   M
                 </button>
-                <button className="w-[72px] h-11 border border-[#E5E7EB] rounded-xl">
+                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-[#E5E7EB] rounded-xl">
                   L
                 </button>
-                <button className="w-[72px] h-11 border border-[#E5E7EB] rounded-xl">
+                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-[#E5E7EB] rounded-xl">
                   XL
                 </button>
-                <button className="w-[72px] h-11 border border-[#E5E7EB] rounded-xl">
+                <button className="w-[68px] lg:w-[72px] h-9 lg:h-11 border border-[#E5E7EB] rounded-xl">
                   2XL
                 </button>
               </div>
@@ -129,23 +130,25 @@ const ProductDetails = () => {
               <div className="flex justify-between"> 
 
                 {/* QTY */}
-                <div className="py-2 px-3 rounded-[9999px] bg-[#E5E7EB] flex gap-4 items-center">
-                    <button className="w-6 h-6 rounded-full border border-[#E5E7EB] bg-white flex justify-center items-center">
-                        <FaMinus className="text-sm text-primary font-normal"/>
+                <div className="py-[2px] lg:py-2 px-[6px] lg:px-3 rounded-[9999px] bg-[#E5E7EB] flex gap-4 items-center">
+                    <button className="w-5 lg:w-6 h-5 lg:h-6 rounded-full border border-[#E5E7EB] bg-white flex justify-center items-center">
+                        <FaMinus className="text-[12px] lg:text-sm text-primary font-normal"/>
                     </button>
                     <h2 className="text-base text-primary font-medium font-popppind">1</h2>
-                    <button className="w-6 h-6 rounded-full border border-[#E5E7EB] bg-white flex justify-center items-center">
-                        <FaPlus className="text-sm text-primary"/>
+                    <button className="w-5 lg:w-6 h-5 lg:h-6 rounded-full border border-[#E5E7EB] bg-white flex justify-center items-center">
+                        <FaPlus className="text-[12px] lg:text-sm text-primary"/>
                     </button>
                 </div>
 
                 {/* Cart button */}
 
-                <button className="py-4 px-8 bg-primary text-base text-white font-medium font-popppind rounded-[9999px] flex justify-center items-center gap-2"> 
+                <button className="py-[10px] lg:py-4 px-[24px] lg:px-8 bg-primary text-sm lg:text-base text-white font-medium font-popppind rounded-[9999px] flex justify-center items-center gap-2"> 
                     <HiShoppingBag />
                     Add to cart
                 </button>
               </div>
+
+              <div className="hidden lg:block">
 
               <div className="det flex items-center justify-between mt-8 pb-4 border-b border-b-border">
                 <div className="l-text mb-[10px]">
@@ -157,11 +160,13 @@ const ProductDetails = () => {
                   <p className="text-base text-secoundary font-normal font-popppind">$0</p>
                 </div>
               </div>
-
               <div className="total flex items-center justify-between mt-4">
                 <h2 className="text-base text-primary font-semibold font-popppind">Total</h2>
                 <h2 className="text-base text-primary font-semibold font-popppind">$169.99</h2>
               </div>
+              </div>
+
+
 
 
             </div>
@@ -170,31 +175,32 @@ const ProductDetails = () => {
 
           {/* ---Product description---- */}
 
-          <div className="mt-[108px] flex flex-col gap-15">
+          <div className="mt-[108px] flex flex-col gap-15 px-3 lg:px-0">
             <div>
-            <h2 className="text-4xl text-primary font-semibold font-popppind">Black Automatic Watch</h2>
-            <p className="text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">The St. Louis Meramec Canoe Company was founded by Alfred Wickett in 1922. Wickett had previously worked for the Old Town Canoe Co from 1900 to 1914. Manufacturing of the classic wooden canoes in Valley Park, Missouri ceased in 1978.</p>
+            <h2 className="text-4xl text-primary font-semibold font-popppind hidden lg:block">Black Automatic Watch</h2>
+            <h2 className="text-[20px] lg:text-4xl text-primary font-semibold font-popppind lg:hidden">About this product</h2>
+            <p className="text-sm lg:text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">The St. Louis Meramec Canoe Company was founded by Alfred Wickett in 1922. Wickett had previously worked for the Old Town Canoe Co from 1900 to 1914. Manufacturing of the classic wooden canoes in Valley Park, Missouri ceased in 1978.</p>
 
             </div>
 
             <div>
-            <h2 className="text-4xl text-primary font-semibold font-popppind">Fabric + Care</h2>
-            <p className="text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Material: Soft wool blend</p>
-            <p className="text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Color: Various</p>
+            <h2 className="text-[20px] lg:text-4xl text-primary font-semibold font-popppind">Fabric + Care</h2>
+            <p className="text-sm lg:text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Material: Soft wool blend</p>
+            <p className="text-sm lg:text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Color: Various</p>
 
             </div>
 
             <div>
-            <h2 className="text-4xl text-primary font-semibold font-popppind">Sale performance</h2>
-            <p className="text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Sales: 0</p>
-            <p className="text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Review Count: -</p>
-            <p className="text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Review Average: -</p>
+            <h2 className="text-[20px] lg:text-4xl text-primary font-semibold font-popppind">Sale performance</h2>
+            <p className="text-sm lg:text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Sales: 0</p>
+            <p className="text-sm lg:text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Review Count: -</p>
+            <p className="text-sm lg:text-base text-secoundary font-normal font-popppind max-w-[735px] mt-1">Review Average: -</p>
 
             </div>
 
             <div>
-            <h2 className="text-4xl text-primary font-semibold font-popppind">Keywords</h2>
-            <div className="flex items-center gap-2">
+            <h2 className="text-[20px] lg:text-4xl text-primary font-semibold font-popppind">Keywords</h2>
+            <div className="flex flex-wrap items-center gap-2">
               <div className="px-[14px] py-2 flex border border-border rounded-full mt-2">
                 <BsStars className=""/>
                 <p className="text-[12px] text-secoundary font-normal font-popppind">men's fashion</p>
@@ -224,7 +230,12 @@ const ProductDetails = () => {
         {/* Recommended products */}
 
         <div className="mt-[96px]">
+          <div className="hidden lg:block">
           <CommonHead text1={'Recommended products'}/>
+          </div>
+          <div className="lg:hidden">
+          <CommonHead text1={'Recommendations'}/>
+          </div>
 
           <div className="reco mb-[96px] px-3 lg:px-0">
 
