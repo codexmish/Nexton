4c8471943fd531dd4af932293d5912419e8e90d0
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-30 12:40:18 +0600

    grpcery latest added

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import BeautyLatest from './BeautyLatest'
 import FragrancesLatest from './FragrancesLatest'
 import FurnitureLatest from './FurnitureLatest'
+import GroceriesLatest from './GroceriesLatest'
 
 const LatestProducts = () => {
   return (
     <>
     {/* <BeautyLatest/> */}
     {/* <FragrancesLatest/> */}
-    <FurnitureLatest/>
+    {/* <FurnitureLatest/> */}
+    <GroceriesLatest/>
     
     </>
   )
