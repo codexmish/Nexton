b76134996a7659c712498bb7349aa0e78321849a
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-30 12:50:32 +0600

    latest products added

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import BeautyLatest from './BeautyLatest'
-import FragrancesLatest from './FragrancesLatest'
-import FurnitureLatest from './FurnitureLatest'
-import GroceriesLatest from './GroceriesLatest'
+import React, { useState } from "react";
+import BeautyLatest from "./BeautyLatest";
+import FragrancesLatest from "./FragrancesLatest";
+import FurnitureLatest from "./FurnitureLatest";
+import GroceriesLatest from "./GroceriesLatest";
+
+const categories = [
+  { name: "Beauty", component: BeautyLatest },
+  { name: "Fragnance", component: FragrancesLatest },
+  { name: "Furniture", component: FurnitureLatest },
+  { name: "Groceries", component: GroceriesLatest },
+];
 
 const LatestProducts = () => {
+  const [activeCat, setActiveCat] = useState("Beauty");
+
+  // Active component find
+  const activeObj = categories.find((cat) => cat.name === activeCat);
+  const ActiveComponent = activeObj.component;
+
   return (
     <>
-    {/* <BeautyLatest/> */}
-    {/* <FragrancesLatest/> */}
-    {/* <FurnitureLatest/> */}
-    <GroceriesLatest/>
-    
-    </>
-  )
-}
+      <div className="container">
+        <div className="flex justify-center items-center gap-6 lg:gap-14 mt-2 text-sm lg:text-[18px] text-black font-bold lg:font-normal font-popppind">
+          {categories.map((cat, id) => (
+            <h2
+              key={id}
+              className={`cursor-pointer ${
+                activeCat === cat.name ? "text-red-500" : ""
+              }`}
+              onClick={() => setActiveCat(cat.name)}
+            >
+              {cat.name}
+            </h2>
+          ))}
+        </div>
 
-export default LatestProducts
+        <div>
+          <ActiveComponent />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LatestProducts;
